05fbec0136bca2feed981abe09c0455ceaaa6b83
hfidelin <hfidelin1920@gmail.com>
2024-03-15 19:20:00 +0100

    update 2D

--- dic_sing.py
+++ dic_sing.py
@@ -18,7 +18,7 @@ g = px.Image('zoom-0070_1.tif').Load()
 cam = px.Camera([100, 7, -2.3, 0.1])
 
 spline, ctrl_pts = bs.new_disk([0, 0, 0], [0, 0, 1], 1)
-spline, ctrl_pts = bs.new_quarter_cylinder([0, 0,0], [0, 0, 1], 1, 1)
+#spline, ctrl_pts = bs.new_quarter_cylinder([0, 0,0], [0, 0, 1], 1, 1)
 
 
 
@@ -28,25 +28,29 @@ knotVect = spline.getKnots()
 
 m = BSplinePatch(ctrl_pts, degree, knotVect)
 
+
+# %%
+
 m.Connectivity()
 
-init, xi, eta = m.DICIntegrationPixel(f, m, cam)
+m.DICIntegrationPixelElem(f, m, cam)
 
-xi_init = init[0]
-eta_init = init[1]
+# %% 
+# xi_init = init[0]
+# eta_init = init[1]
 
-P = m.Get_P()
-N = spline.DN(np.array([xi_init, eta_init]), k=[0, 0])
-u_init, v_init = cam.P(N @ P[:, 0], N @ P[:, 1])
-u, v = cam.P(m.pgx, m.pgy)
+# P = m.Get_P()
+# N = spline.DN(np.array([xi_init, eta_init]), k=[0, 0])
+# u_init, v_init = cam.P(N @ P[:, 0], N @ P[:, 1])
+# u, v = cam.P(m.pgx, m.pgy)
 
 
-px.PlotMeshImage(f, m, cam)
-plt.scatter(v, u, c='b', label="Points convergés")
-plt.scatter(v_init, u_init, c='r', label="Initialisation")
-plt.legend()
+# px.PlotMeshImage(f, m, cam)
+# plt.scatter(v, u, c='b', label="Points convergés")
+# plt.scatter(v_init, u_init, c='r', label="Initialisation")
+# plt.legend()
 
-# %% Visualisation de l'espace paramétrique
+# %% 
 
 u, v = cam.P(m.pgx, m.pgy)
 px.PlotMeshImage(f, m, cam)
@@ -54,16 +58,4 @@ plt.scatter(v, u)
 # plt.grid()
 
 
-# %%
-
-xi2 = np.array([0, 1])
-eta2 = np.array([0, 1])
-
-N = spline.DN([xi2, eta2], k=[0, 0])
-
-u2, v2 = cam.P(N @ P[:, 0], N @ P[:, 1])
-px.PlotMeshImage(f, m, cam) 
-plt.scatter(v2, u2, c='r')
-
-# %%
 
